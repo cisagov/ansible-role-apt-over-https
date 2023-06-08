5500b3e35482b4082a2d69244cb95ba04e63dbdd
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-06-08 11:30:35 -0400

    Update test code to ignore non-Debian platforms

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,33 +15,35 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 @pytest.mark.parametrize("pkg", ["apt-transport-https"])
 def test_apt_https_support(host, pkg):
     """Ensure apt has support for repository URLs using HTTPS."""
-    assert host.package(pkg).is_installed
+    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        assert host.package(pkg).is_installed
 
 
 def test_source_list_for_http(host):
     """Check if the source list file has any URLs still using HTTP."""
-    source_file = "/etc/apt/sources.list"
-    # As of Debian Bullseye the /etc/apt/sources.list file has moved
-    # to /etc/apt/sources.list.d/debian.sources
-    if (
-        host.system_info.distribution == "debian"
-        and host.system_info.codename == "bookworm"
-    ):
-        source_file = "/etc/apt/sources.list.d/debian.sources"
-
-    file_lines = host.file(source_file).content_string.split(os.linesep)
-    sources = [line for line in file_lines if line.startswith("deb")]
-
-    for source in sources:
-        source_elements = source.split(" ")
-        # The second element of a source entry can optionally be a set of options
-        # in the form "[options]".
-        repository_url = source_elements[
-            1 if not source_elements[1].startswith("[") else 2
-        ]
-        if host.system_info.distribution == "ubuntu" and (
-            "ubuntu.com" in repository_url or "canonical.com" in repository_url
+    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        source_file = "/etc/apt/sources.list"
+        # As of Debian Bullseye the /etc/apt/sources.list file has
+        # moved to /etc/apt/sources.list.d/debian.sources
+        if (
+            host.system_info.distribution == "debian"
+            and host.system_info.codename == "bookworm"
         ):
-            # We do not modify the standard repositories on Ubuntu systems
-            continue
-        assert repository_url.startswith("http://") is False
+            source_file = "/etc/apt/sources.list.d/debian.sources"
+
+        file_lines = host.file(source_file).content_string.split(os.linesep)
+        sources = [line for line in file_lines if line.startswith("deb")]
+
+        for source in sources:
+            source_elements = source.split(" ")
+            # The second element of a source entry can optionally be a
+            # set of options in the form "[options]".
+            repository_url = source_elements[
+                1 if not source_elements[1].startswith("[") else 2
+            ]
+            if host.system_info.distribution == "ubuntu" and (
+                "ubuntu.com" in repository_url or "canonical.com" in repository_url
+            ):
+                # We do not modify the standard repositories on Ubuntu systems
+                continue
+            assert repository_url.startswith("http://") is False
