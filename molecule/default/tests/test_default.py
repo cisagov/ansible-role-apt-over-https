2bcc50d0b28b786bc83c94e9c024e8df75c34730
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2023-06-12 11:15:40 -0400

    Add a test to ensure that apt update does not indicate any unexpected HTTP URLs

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -47,3 +47,34 @@ def test_source_list_for_http(host):
                 # We do not modify the standard repositories on Ubuntu systems
                 continue
             assert repository_url.startswith("http://") is False
+
+
+def test_apt_update(host):
+    """Check if the output of apt update indicates any unexpected HTTP URLs."""
+    if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
+        cmd = host.run("apt update")
+        assert cmd.succeeded
+        # Typical output looks like:
+        # $ apt update
+        # Get:1 http://security.ubuntu.com/ubuntu jammy-security InRelease [110 kB]
+        # Get:2 http://archive.ubuntu.com/ubuntu jammy InRelease [270 kB]
+        # Get:3 http://security.ubuntu.com/ubuntu jammy-security/universe amd64 Packages [928 kB]
+        # Get:4 http://archive.ubuntu.com/ubuntu jammy-updates InRelease [119 kB]
+        # <snip>
+        # Get:18 http://archive.ubuntu.com/ubuntu jammy-backports/main amd64 Packages [49.4 kB]
+        # Fetched 24.9 MB in 3s (9622 kB/s)
+        # Reading packlists... Done
+        # Building dependency tree... Done
+        # Reading state information... Done
+        # 7 packages can be upgraded. Run 'apt list --upgradable' to see them.
+        output = cmd.stdout
+        sources = [line for line in output if line.startswith("Get:")]
+        for source in sources:
+            repository_url = source.split(" ")[1]
+            if host.system_info.distribution == "ubuntu" and (
+                "ubuntu.com" in repository_url or "canonical.com" in repository_url
+            ):
+                # The standard repositories on Ubuntu systems are
+                # HTTP-only
+                continue
+            assert repository_url.startswith("http://") is False
