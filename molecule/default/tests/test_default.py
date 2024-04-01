8f3c685628991fb4d223e33ee46cfcb99ac7312c
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2024-04-01 16:09:50 -0400

    Further adjustment of SourceList file testing
    Approach testing just as we do for applying source changes. Check if an
    applicable file exists and verify the contents if it does.
    
    Co-authored-by: Shane Frasier <jeremy.frasier@gwe.cisa.dhs.gov>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,6 +4,7 @@
 import os
 
 # Third-Party Libraries
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -11,35 +12,31 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_source_list_for_http(host):
+@pytest.mark.parametrize(
+    "sourcelist_file",
+    ["/etc/apt/sources.list", "/etc/apt/sources.list.d/debian.sources"],
+)
+def test_source_list_for_http(host, sourcelist_file):
     """Check if the source list file has any URLs still using HTTP."""
     if host.system_info.distribution in ["debian", "kali", "ubuntu"]:
-        if host.system_info.distribution in ["kali", "ubuntu"]:
-            source_file = "/etc/apt/sources.list"
-        if host.system_info.distribution == "debian":
-            # As of Debian Bookworm the /etc/apt/sources.list file has
-            # moved to /etc/apt/sources.list.d/debian.sources
-            source_file = "/etc/apt/sources.list.d/debian.sources"
+        target_file = host.file(sourcelist_file)
+        if target_file.exists:
+            file_lines = target_file.content_string.split(os.linesep)
+            sources = [line for line in file_lines if line.startswith("deb")]
 
-            if host.system_info.codename in ["buster", "bullseye"]:
-                source_file = "/etc/apt/sources.list"
-
-        file_lines = host.file(source_file).content_string.split(os.linesep)
-        sources = [line for line in file_lines if line.startswith("deb")]
-
-        for source in sources:
-            source_elements = source.split(" ")
-            # The second element of a source entry can optionally be a
-            # set of options in the form "[options]".
-            repository_url = source_elements[
-                1 if not source_elements[1].startswith("[") else 2
-            ]
-            if host.system_info.distribution == "ubuntu" and (
-                "ubuntu.com" in repository_url or "canonical.com" in repository_url
-            ):
-                # We do not modify the standard repositories on Ubuntu systems
-                continue
-            assert repository_url.startswith("http://") is False
+            for source in sources:
+                source_elements = source.split(" ")
+                # The second element of a source entry can optionally be a
+                # set of options in the form "[options]".
+                repository_url = source_elements[
+                    1 if not source_elements[1].startswith("[") else 2
+                ]
+                if host.system_info.distribution == "ubuntu" and (
+                    "ubuntu.com" in repository_url or "canonical.com" in repository_url
+                ):
+                    # We do not modify the standard repositories on Ubuntu systems
+                    continue
+                assert repository_url.startswith("http://") is False
 
 
 def test_apt_update(host):
